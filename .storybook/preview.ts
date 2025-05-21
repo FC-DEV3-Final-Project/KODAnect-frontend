49a0647dd630c18ad117497815b23cff441b1be5
ijisun <ijisun9292@gmail.com>
2025-05-21 13:48:23 +0900

    chore: storybook 기본 설정

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from "@storybook/react";
+import "@/index.css"; // Tailwind CSS 포함된 글로벌 스타일 경로
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
