db53a0bf5b523d09326279e90c93b9da4eb2ee5a
ijisun <ijisun9292@gmail.com>
2025-05-21 21:54:48 +0900

    chore: 불필요한 Tailwind content 경로 제거

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@krds-ui/**/*.{js,ts,jsx,tsx}", // KRDS 패키지
-    "./.storybook/**/*.{js,ts,jsx,tsx}", // Storybook
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
