390ab9d568b9bd2f9b0a5682934f5fccc5b1dc7f
mirinae700 <mirinae219@gmail.com>
2025-05-23 10:41:02 +0900

    chore: storybook viewport 설정 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,24 @@
 import type { Preview } from "@storybook/react";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 import "@/shared/styles/index.css"; // Tailwind CSS 포함된 글로벌 스타일 경로
 import "@krds-ui/core/dist/style.css";
+
+const KODA_VIEWPORTS = {
+  kodaPC: {
+    name: "KODA PC",
+    styles: {
+      width: "1200px",
+      height: "100%",
+    },
+  },
+  kodaMobile: {
+    name: "KODA Mobile",
+    styles: {
+      width: "767px",
+      height: "100%",
+    },
+  },
+};
 
 const preview: Preview = {
   parameters: {
@@ -8,6 +26,12 @@ const preview: Preview = {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
+      },
+    },
+    viewport: {
+      viewports: {
+        ...KODA_VIEWPORTS,
+        ...MINIMAL_VIEWPORTS,
       },
     },
   },
