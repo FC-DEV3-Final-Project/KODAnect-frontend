cb812d60ae1a7deab9afc0b1f00ec4f73e6cc55f
mirinae700 <mirinae219@gmail.com>
2025-05-23 17:12:47 +0900

    refactor: Header 구조 개선 - inner 추가 및 padding, 최대 너비 적용

--- src/widget/Header.tsx
+++ src/widget/Header.tsx
@@ -9,23 +9,26 @@ export default function Header() {
 
   return (
     <>
-      <header className="flex h-full w-full justify-between border-b-[1px] border-[#CDD1D5] bg-white p-p6 md:items-end">
-        <LinkButton link="/" title="홈으로">
-          <h1>
-            <img src={Logo} className="h-[4rem] md:h-[6rem]" alt="한국장기조직기증원 로고" />
-          </h1>
-        </LinkButton>
-        <button
-          type="button"
-          className="flex cursor-pointer items-center gap-g3 px-p5 py-p4"
-          title="전체메뉴"
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-        >
-          <img src={Menu} className="w-[2rem]" alt="메뉴 아이콘" />
-          <h2 className="text-b-sm font-bold">전체메뉴</h2>
-        </button>
+      <header className="h-full w-full border-b-[1px] border-[#CDD1D5] bg-white">
+        {/* Header Inner */}
+        <div className="m-auto flex max-w-[1280px] justify-between p-p6 md:px-p10">
+          <LinkButton link="/" title="홈으로">
+            <h1>
+              <img src={Logo} className="h-[4rem] md:h-[6rem]" alt="한국장기조직기증원 로고" />
+            </h1>
+          </LinkButton>
+          <button
+            type="button"
+            className="flex cursor-pointer items-center gap-g3 px-p5 py-p4"
+            title="전체메뉴"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            <img src={Menu} className="w-[2rem]" alt="메뉴 아이콘" />
+            <h2 className="text-b-sm font-bold">전체메뉴</h2>
+          </button>
+        </div>
       </header>
       {isOpen && <Navbar />}
     </>
