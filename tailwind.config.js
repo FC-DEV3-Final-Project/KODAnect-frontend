cdb3200c80d2ab2268898882b8233540f6b8fc98
ijisun <ijisun9292@gmail.com>
2025-05-24 20:35:24 +0900

    feat: Tailwind config에 mobile 브레이크포인트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      // 모바일 전용 스타일에 사용할 브레이크포인트
+      mobile: { max: "767px" },
+    },
     extend: {},
   },
   plugins: [require("./plugins/krds-theme.js")],
