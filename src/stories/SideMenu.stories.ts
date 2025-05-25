ad07a85550b2404f79ea33ec19c165d296653eee
ijisun <ijisun9292@gmail.com>
2025-05-25 23:28:03 +0900

    feat: SideMenu.stories.ts 추가

---
+++ src/stories/SideMenu.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import SideMenu from "@/widget/SideMenu";
+// 추후 sitemenu.ts 파일 merge되면 경로 수정 예정
+import { SITE_MENU } from "@/shared/constant/mockSiteMenu";
+
+const meta = {
+  title: "KRDS/SideMenu",
+  component: SideMenu,
+  parameters: {
+    layout: "fullscreen",
+    backgrounds: {
+      default: "light ",
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof SideMenu>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    selectedLabel: SITE_MENU[0]?.label ?? "장기·조직기증",
+  },
+};
