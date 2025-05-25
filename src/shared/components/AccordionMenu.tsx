a3246c4608dd6ba13bc48ea21f7c1cdc88465cdd
ijisun <ijisun9292@gmail.com>
2025-05-25 23:19:57 +0900

    feat: 아코디언 형태의 메뉴 컴포넌트 AccordionMenu 생성

---
+++ src/shared/components/AccordionMenu.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Arrow from "@/assets/icon/arrow-down.svg";
+
+interface SiteMenu {
+  label: string;
+  path?: string;
+  children?: SiteMenu[];
+}
+
+function AccordionMenu({ menu, defaultOpen = true }: { menu: SiteMenu; defaultOpen?: boolean }) {
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+
+  const hasChildren = menu.children && menu.children.length > 0;
+
+  return (
+    <div className="border-b border-gray-200 text-b-md">
+      {/* Toggle button for accordion */}
+      <button
+        className="flex w-full items-center justify-between px-p3 py-p6 font-bold text-gray-95"
+        onClick={() => setIsOpen(!isOpen)}
+        aria-expanded={isOpen}
+        aria-label={`${menu.label} 하위 메뉴 ${isOpen ? "닫기" : "펼치기"}`}
+      >
+        {menu.label}
+        {hasChildren && (
+          <img
+            src={Arrow}
+            alt={isOpen ? "펼치기 아이콘" : "접기 아이콘"}
+            className={`w-6 transition-transform duration-300 mobile:w-5 ${isOpen ? "rotate-180" : "rotate-0"} `}
+          />
+        )}
+      </button>
+
+      {/* Submenu list */}
+      {hasChildren && isOpen && (
+        <ul className="py-p3" aria-label={`${menu.label} 하위 메뉴`}>
+          {menu.children!.map((child, index) => (
+            <li
+              key={index}
+              className="hover:rounded-r2 hover:bg-secondary-5 hover:font-bold hover:text-secondary-80"
+            >
+              <a
+                href={child.path || "#"}
+                onClick={() => {
+                  alert(`${child.label} 메뉴 클릭`);
+                }}
+                className="block px-p6 py-p3"
+              >
+                {child.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default AccordionMenu;
