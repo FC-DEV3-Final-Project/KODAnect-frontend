300f78a56e546f95ae329430e97bdec848981e2a
mirinae700 <mirinae219@gmail.com>
2025-06-01 23:34:36 +0900

    feat: 메뉴 관련 store, type, style 추가

--- src/widget/header/menuStyles.ts
+++ src/widget/header/menuStyles.ts
@@ -1,4 +1,17 @@
+// PC
 export const baseMenuStyle = "flex items-center gap-g3 hover:bg-secondary-5 active:bg-secondary-10";
 export const baseIconStyle = "w-8";
+
+// All Menu
+export const overlayStyle =
+  "fixed inset-0 z-50 bg-black bg-opacity-25 flex items-center justify-center";
+export const modalStyle = "m-g9 w-full rounded-r6 bg-white";
+
+// Mobile
+export const baseMobileMenuStyle = "flex items-center";
+export const baseMobileDepth2Style =
+  "border-b border-gray-40 px-p3 pb-p5 pt-p2 text-b-lg font-bold";
+
+// Common
 export const menuDotStyle =
-  "before:block before:h-[4px] before:w-[4px] before:rounded-full before:bg-gray-80 before:content-['']";
+  "before:block before:shrink-0 before:h-[4px] before:w-[4px] before:rounded-full before:bg-gray-80 before:content-['']";
