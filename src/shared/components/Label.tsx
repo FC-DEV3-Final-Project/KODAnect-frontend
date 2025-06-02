288b0fe6895ed763c24f983738d2f38910446f18
jiwon <103546376+eas1w0n@users.noreply.github.com>
2025-06-02 14:42:03 +0900

    Feat: textinput & textarea 컴포넌트 제작 (#9)
    * feat: input 컴포넌트 생성
    
    * feat: textinput storybook 작성
    
    * feat: textarea 컴포넌트 제작
    
    * chore: textarea type 분리
    
    * feat: textarea storybook 생성
    
    * chore: textarea type 분리
    
    * chore: 컴포넌트 타입 위치 이동
    
    * refactor: textinput & textarea 컴포넌트 사이즈 유동적 적용
    
    * chore: textinput error border 컬러 수치값 명시
    
    * chore: textarea error border 컬러 수치값 명시
    
    * chore: dev 최신화 반영 및 textinput & area focus border 컬러 수치값 명시

---
+++ src/shared/components/Label.tsx
@@ -0,0 +1,40 @@
+type LabelProps<E extends React.ElementType> = {
+  size?: "m" | "s";
+  weight?: "regular" | "bold";
+  color?: "default" | "danger" | "success" | "info" | "length";
+  children: React.ReactNode;
+  className?: string;
+} & React.ComponentPropsWithoutRef<E>;
+
+export const Label = <E extends React.ElementType = "label">({
+  size = "m",
+  weight = "regular",
+  color = "default",
+  children,
+  className = "",
+  ...props
+}: LabelProps<E>) => {
+  const sizeClass = {
+    m: "text-b-sm",
+    s: "text-b-xs",
+  }[size];
+
+  const weightClass = {
+    regular: "font-regular",
+    bold: "font-bold",
+  }[weight];
+
+  const textColorClass = {
+    default: "text-gray-70",
+    danger: "text-danger-60",
+    success: "text-success-60",
+    info: "text-information-60",
+    length: "text-primary-60",
+  }[color];
+
+  return (
+    <label className={`${sizeClass} ${weightClass} ${textColorClass} ${className}`} {...props}>
+      {children}
+    </label>
+  );
+};
