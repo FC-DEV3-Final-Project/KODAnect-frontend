1e1c3efac7bb0b55ab951ad76ee4be673518e92c
jiwonnayoung <lny1203@naver.com>
2025-06-02 17:32:52 +0900

    chore: 테스트 코드 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,3 @@
-import { useState } from "react";
-import DatePicker from "@/shared/components/calendar/DatePicker";
-
 export default function Home() {
-  const [range, setRange] = useState<{ from: Date | null; to: Date | null }>({
-    from: null,
-    to: null,
-  });
-  return (
-    <div>
-      Home
-      <DatePicker range={range} onRangeChange={setRange} />
-    </div>
-  );
+  return <div>Home</div>;
 }
