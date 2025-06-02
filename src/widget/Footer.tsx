471cdd207df79fbccf74c8059cf66ffd9aa04300
ijisun <ijisun9292@gmail.com>
2025-06-02 11:07:39 +0900

    refactor: 상수 분리

--- src/widget/Footer.tsx
+++ src/widget/Footer.tsx
@@ -1,54 +1,11 @@
+import { useState } from "react";
 import Logo from "@/assets/logo.svg";
-import Youtube from "@/assets/icon/youtube.svg";
-import FaceBook from "@/assets/icon/facebook.svg";
-import Blog from "@/assets/icon/naver-blog.svg";
-import Instagram from "@/assets/icon/instagram.svg";
 import Arrow from "@/assets/icon/arrow-down.svg";
 import OpenNewWindow from "@/assets/icon/open-new-window.svg";
-import { useState } from "react";
+import { RELATED_SITES, SOCIAL_LINKS, POLICY_LINKS } from "@/shared/constant/footerlink";
 
 function Footer() {
   const [isOpen, setIsOpen] = useState(false);
-
-  const relatedSites = [
-    { title: "보건복지부", url: "https://www.mohw.go.kr/" },
-    { title: "보건복지 상담센터", url: "https://www.129.go.kr/" },
-    { title: "국립장기조직 혈액관리원", url: "https://www.konos.go.kr/" },
-    { title: "대한이식학회", url: "https://mykst.org/" },
-  ];
-  const socialLinks = [
-    {
-      href: "https://www.youtube.com/channel/UCbhsMbBEOm1vIpOZXbaZR7g",
-      icon: Youtube,
-      label: "유튜브",
-      alt: "한국장기조직기증원 유튜브 바로가기",
-    },
-    {
-      href: "https://www.facebook.com/%ED%95%9C%EA%B5%AD%EC%9E%A5%EA%B8%B0%EC%A1%B0%EC%A7%81%EA%B8%B0%EC%A6%9D%EC%9B%90-102968767953316/?ref=pages_you_manage",
-      icon: FaceBook,
-      label: "페이스북",
-      alt: "한국장기조직기증원 페이스북 바로가기",
-    },
-    {
-      href: "https://blog.naver.com/koda1458",
-      icon: Blog,
-      label: "네이버 블로그",
-      alt: "한국장기조직기증원 네이버 블로그 바로가기",
-    },
-    {
-      href: "https://www.instagram.com/koda1458/?hl=ko",
-      icon: Instagram,
-      label: "인스타그램",
-      alt: "한국장기조직기증원 인스타그램 바로가기",
-    },
-  ];
-  const policyLinks = [
-    { title: "개인정보처리방침", url: "https://koda1458.kr/footer/privacy.do" },
-    { title: "위치안내", url: "#" },
-    { title: "경영고시", url: "#" },
-    { title: "기증희망등록", url: "/donation/donation-registration/empty" },
-    { title: "저작권정책", url: "#" },
-  ];
 
   // layout.tsx 혹은 page.tsx 같은 상위 컴포넌트를 flex로 감싸줘야 footer 하단에 고정됩니다.
   /*
@@ -84,7 +41,7 @@ function Footer() {
             isOpen ? "border-x border-t border-gray-20 py-g6" : "max-h-0"
           }`}
         >
-          {relatedSites.map(({ title, url }) => (
+          {RELATED_SITES.map(({ title, url }) => (
             <li key={title} className="mb-p4 last:mb-0 hover:bg-gray-5 hover:font-bold mobile:mb-0">
               <a
                 href={url}
@@ -136,7 +93,7 @@ function Footer() {
             {/* Footer Link */}
             <nav aria-label="SNS 바로가기">
               <ul className="flex gap-g3">
-                {socialLinks.map(({ href, icon, label, alt }) => (
+                {SOCIAL_LINKS.map(({ href, icon, label, alt }) => (
                   <li key={label}>
                     <a
                       href={href}
@@ -160,7 +117,7 @@ function Footer() {
           >
             <nav aria-label="법적 고지사항">
               <ul className="flex flex-wrap gap-g3 text-gray-90">
-                {policyLinks.map(({ title, url }) => {
+                {POLICY_LINKS.map(({ title, url }) => {
                   const isExternal = url.startsWith("http");
 
                   return (
