520a383b83926b005a7122451bc89a06b8a0a026
ijisun <ijisun9292@gmail.com>
2025-06-02 11:44:26 +0900

    refactor: 상수 분리

---
+++ src/shared/constant/topvisual.ts
@@ -0,0 +1,5 @@
+export const TOP_VISUAL = [
+  { label: "장기·조직기증", imageUrl: "/src/assets/images/organ.png" },
+  { label: "참여·정보", imageUrl: "/src/assets/images/participation.png" },
+  { label: "홍보·알림", imageUrl: "/src/assets/images/announcement.png" },
+];
