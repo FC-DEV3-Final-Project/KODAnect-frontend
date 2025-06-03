3b3bf80b1765cb0596b94df4eefe6a1be0dc8c83
박현아 <pha3113@naver.com>
2025-06-03 19:54:14 +0900

    Feat: breadcrumb 컴포넌트 추가 (#21)
    * feat: button 수정 및 storybook size 추가
    
    * Rename: button type 컴포넌트 내부로 이동
    
    * remove: button 컴포넌트 text 타입 제거
    
    * feat: breadcrumb 컴포넌트 추가
    
    * style: icon size 클래스 사용 변경
    
    * style: icon rotate className으로 적용
    
    * feat: breadcrumb 라우팅 구조와 연동

--- src/shared/types/Button.types.ts
+++
@@ -1,8 +0,0 @@
-export type ButtonProps<E extends React.ElementType> = {
-  type?: "button" | "submit" | "reset";
-  variant?: "primary" | "secondary" | "tertiary" | "text";
-  size?: "x-small" | "small" | "medium" | "large" | "x-large";
-  children: React.ReactNode;
-  className?: string;
-  disabled?: boolean;
-} & React.ComponentPropsWithoutRef<E>;
