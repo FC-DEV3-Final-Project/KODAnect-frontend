403959a059f32ffae94af6f5bd230174cdffc037
jiwonnayoung <lny1203@naver.com>
2025-06-04 16:41:01 +0900

    chore: 테스트 코드 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,3 @@
-import CommentArea from "@/shared/components/comment/CommentArea";
-
 export default function Home() {
-  return (
-    <div>
-      Home
-      <CommentArea />
-    </div>
-  );
+  return <div>Home</div>;
 }
