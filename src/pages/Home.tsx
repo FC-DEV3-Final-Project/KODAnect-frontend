daecefb5b55a34f76d6e8582ffbfd0a9b10893d7
jiwonnayoung <lny1203@naver.com>
2025-06-05 00:23:18 +0900

    feat: 검색어 삭제 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,3 @@
-import SearchInput from "@/shared/components/SearchInput";
-
 export default function Home() {
-  return (
-    <div>
-      Home
-      <div className="w-[31.3rem]">
-        <SearchInput
-          onSubmit={(keyword: string) => {
-            console.log(keyword);
-            // 여기에 검색 결과 필터링 등 실행
-          }}
-          placeholder="검색어를 입력해 주세요"
-        />
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 }
--- src/shared/components/SearchInput.tsx
+++ src/shared/components/SearchInput.tsx
@@ -1,6 +1,20 @@
 import { useState } from "react";
 import type { FormEvent } from "react";
 import SearchIcon from "@/assets/icon/search.svg?react";
+import DeleteIcon from "@/assets/icon/delete.svg?react";
+
+/**
+ * Example usage:
+ *
+ * <div className="w-[31.3rem]">
+ *   <SearchInput
+ *     onSubmit={(keyword: string) => {
+ *       // 여기에 검색 결과 필터링 등 실행
+ *     }}
+ *     placeholder="검색어를 입력해 주세요"
+ *   />
+ * }
+ */
 
 type SearchInputProps = {
   placeholder: string;
@@ -12,6 +26,13 @@ function SearchInput({ onSubmit, placeholder }: SearchInputProps) {
   const handleSubmit = (e?: FormEvent) => {
     e?.preventDefault();
     onSubmit(inputValue);
+  };
+
+  const handleClear = () => setInputValue("");
+
+  const getPaddingRight = () => {
+    if (!inputValue) return "pr-p10"; // SearchIcon만 있을 때
+    return "pr-[6.5rem]"; // DeleteIcon + SearchIcon 있을 때
   };
 
   return (
@@ -26,9 +47,19 @@ function SearchInput({ onSubmit, placeholder }: SearchInputProps) {
           value={inputValue}
           placeholder={placeholder}
           onChange={(e) => setInputValue(e.target.value)}
-          className="w-full rounded-r3 border border-gray-60 py-p4 pl-p6 pr-p10 text-b-md text-gray-90 placeholder:text-gray-40 focus:outline-2 focus:outline-primary-50"
+          className={`${getPaddingRight()} w-full rounded-r3 border border-gray-60 py-p4 pl-p6 text-b-md text-gray-90 placeholder:text-gray-40 focus:outline-2 focus:outline-primary-50`}
         />
-
+        {/* 삭제 버튼 */}
+        {inputValue && (
+          <DeleteIcon
+            onClick={handleClear}
+            role="button"
+            tabIndex={0}
+            aria-label="입력값 지우기"
+            className="absolute right-[4rem] top-1/2 h-icon3 w-icon3 -translate-y-1/2 cursor-pointer"
+          />
+        )}
+        {/* 검색 버튼 */}
         <SearchIcon
           onClick={() => handleSubmit()}
           role="button"
