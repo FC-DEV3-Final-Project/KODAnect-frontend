9deb49518be043697b361451c07b14868766fecc
mirinae700 <mirinae219@gmail.com>
2025-06-06 02:01:50 +0900

    refactor: 테이블 반응형 스타일 수정

--- src/features/home/component/koda-news/Table.tsx
+++ src/features/home/component/koda-news/Table.tsx
@@ -18,7 +18,7 @@ export default function Table({ items, label }: TableProps) {
       <caption className="sr-only">{label}</caption>
       <colgroup>
         <col />
-        <col width="110px" />
+        <col style={{ width: "110px" }} />
       </colgroup>
       <thead className="sr-only">
         <tr>
@@ -28,15 +28,15 @@ export default function Table({ items, label }: TableProps) {
       </thead>
       <tbody>
         {items.map((item, index) => (
-          <tr key={index}>
+          <tr key={index} className="mobile:flex mobile:flex-col mobile:gap-g1">
             <td
-              className="max-w-[300px] overflow-hidden text-ellipsis whitespace-nowrap px-p2 py-p4 text-gray-90"
+              className="max-w-[300px] mobile:max-w-[275px] overflow-hidden text-ellipsis whitespace-nowrap px-p2 py-p4 text-gray-90 mobile:pb-0"
               title={item.title} // 마우스 올리면 전체 제목 보이도록
             >
               {/* 상세 페이지 이동 필요하면 articleSeq, Link 사용하면 될것 같습니다 */}
               {item.title}
             </td>
-            <td className="p-p2 text-right text-gray-40">{item.writeDate}</td>
+            <td className="p-p2 text-right text-gray-40 mobile:text-left mobile:py-0">{item.writeDate}</td>
           </tr>
         ))}
       </tbody>
