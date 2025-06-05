81bc37db31cde57cc8d1ebbcfb1234b31cc3dc6f
jiwonnayoung <lny1203@naver.com>
2025-06-06 01:12:22 +0900

    chore: 웹 접근성 고려 aria 속성 추가

--- src/shared/components/TopVisual.tsx
+++ src/shared/components/TopVisual.tsx
@@ -53,13 +53,17 @@ function TopVisual({ type }: TopVisualProps) {
   const imgUrl = isMobile && content.mobileImgUrl ? content.mobileImgUrl : content.imgUrl;
 
   return (
-    <header className="relative mx-auto h-[27.6rem] max-w-[1920px] overflow-hidden mobile:h-[32rem]">
+    <header
+      className="relative mx-auto h-[27.6rem] max-w-[1920px] overflow-hidden mobile:h-[32rem]"
+      role="region"
+      aria-label="상단 비주얼 영역"
+    >
       {/* 배경 이미지*/}
 
       {content.isRepeatImage ? (
         <div className="absolute inset-0 flex" aria-hidden="true">
-          <img src={content.imgUrl} alt="" className="h-full w-[99rem] object-cover" />
-          <img src={content.imgUrl} alt="" className="h-full w-[93.1rem] object-cover" />
+          <img src={content.imgUrl} className="h-full w-[99rem] object-cover" />
+          <img src={content.imgUrl} className="h-full w-[93.1rem] object-cover" />
           {/* 검정 오버레이 */}
           <div className="pointer-events-none absolute inset-0 bg-black opacity-60" />
         </div>
@@ -73,10 +77,16 @@ function TopVisual({ type }: TopVisualProps) {
 
       {/* 텍스트 콘텐츠 */}
       <div className="relative z-10 flex h-full flex-col items-center justify-center text-center text-white mobile:items-start mobile:px-p9 mobile:text-left">
-        <h1 className="text-d-md font-bold leading-tight tracking-1 mobile:text-d-md">
+        <h1
+          className="text-d-md font-bold leading-tight tracking-1 mobile:text-d-md"
+          aria-level={1}
+        >
           {content.title}
         </h1>
-        <p className="mt-g3 text-b-lg leading-snug mobile:max-w-[27.5rem] mobile:text-b-sm">
+        <p
+          className="mt-g3 text-b-lg leading-snug mobile:max-w-[27.5rem] mobile:text-b-sm"
+          aria-describedby="description"
+        >
           {Array.isArray(content.description)
             ? content.description.map((line, i) => (
                 <span key={i}>
