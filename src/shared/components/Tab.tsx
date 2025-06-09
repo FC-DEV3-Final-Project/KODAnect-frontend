c4107f4edf576c3272d76f4e108aa4ccdc29efc9
박현아 <pha3113@naver.com>
2025-06-09 11:58:34 +0900

    Feat: tab 컴포넌트 추가 (#41)
    * feat: tab 컴포넌트 추가
    
    * style: 함수형 컴포넌트 방식으로 변경

---
+++ src/shared/components/Tab.tsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import clsx from "clsx";
+
+interface TabProps {
+  label: string;
+  content: React.ReactNode;
+}
+
+interface TabsProps {
+  tabs: TabProps[];
+}
+
+interface TabButtonProps {
+  label: string;
+  isSelected: boolean;
+  onClick: () => void;
+  id: string;
+  panelId: string;
+}
+
+interface TabPanelProps {
+  children: React.ReactNode;
+  id: string;
+  tabId: string;
+  isSelected: boolean;
+}
+
+function TabButton({ label, isSelected, onClick, id, panelId }: TabButtonProps) {
+  return (
+    <button
+      role="tab"
+      aria-selected={isSelected}
+      aria-controls={panelId}
+      id={id}
+      onClick={onClick}
+      className={clsx(
+        "h-full w-full flex-auto cursor-pointer p-[14px] text-h-sm font-bold mobile:p-[12px] mobile:text-h-2xs",
+        isSelected ? "bg-secondary-80 text-gray-0" : "bg-gray-0 text-gray-70",
+      )}
+    >
+      {label}
+      {isSelected && <span className="sr-only">선택됨</span>}
+    </button>
+  );
+}
+
+function TabPanel({ children, id, tabId, isSelected }: TabPanelProps) {
+  return (
+    <div
+      role="tabpanel"
+      id={id}
+      aria-labelledby={tabId}
+      className={`${isSelected ? "" : "hidden"}`}
+    >
+      {children}
+    </div>
+  );
+}
+
+export function Tab({ tabs }: TabsProps) {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  return (
+    <>
+      <ul
+        role="tablist"
+        aria-label="Tab Navigation"
+        className="flex w-full flex-wrap overflow-hidden rounded-r3 border border-gray-30"
+      >
+        {tabs.map((tab, index) => {
+          const isFirstLine = index === 0 || index === 1;
+          const isLast = index === tabs.length - 1;
+
+          return (
+            <li
+              key={index}
+              className={clsx(
+                "w-1/4 border-gray-30 mobile:w-1/2",
+                isFirstLine ? "mobile:border-0" : "mobile:border-t",
+                isLast ? "border-0" : "border-r mobile:border-0 mobile:odd:border-r",
+              )}
+            >
+              <TabButton
+                label={tab.label}
+                isSelected={index === selectedTab}
+                onClick={() => setSelectedTab(index)}
+                id={`tab-${index}`}
+                panelId={`panel-${index}`}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      {tabs.map((tab, index) => (
+        <TabPanel
+          key={index}
+          id={`panel-${index}`}
+          tabId={`tab-${index}`}
+          isSelected={index === selectedTab}
+        >
+          {tab.content}
+        </TabPanel>
+      ))}
+    </>
+  );
+}
