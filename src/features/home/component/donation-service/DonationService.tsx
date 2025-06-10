6cc6eede26a9554085355d9141d1746af6531670
ijisun <ijisun9292@gmail.com>
2025-06-10 17:23:56 +0900

    feat: 기증자 관련 서비스 카드에 아이콘 추가 및 메인 컬러 변경

--- src/features/home/component/donation-service/DonationService.tsx
+++ src/features/home/component/donation-service/DonationService.tsx
@@ -2,12 +2,15 @@ import clsx from "clsx";
 import DonorMemorial from "./DonorMemorial";
 import QuickLinkCard from "./QuickLinkCard";
 
+import donate from "@/assets/images/donate-icon.png";
+import mailBox from "@/assets/images/mainBox-icon.png";
+
 export default function DonationService() {
   return (
     <section
       className={clsx(
         "m-auto mb-g11 grid max-w-[1280px] grid-cols-3 gap-g7 px-p10 text-gray-90",
-        "mobile:mb-g8 mobile:flex mobile:flex-col mobile:gap-g5 mobile:px-p6",
+        "mobile:mb-g8 mobile:flex mobile:flex-col mobile:gap-g6 mobile:px-p6",
       )}
       aria-labelledby="donation-service-heading"
     >
@@ -15,12 +18,14 @@ export default function DonationService() {
         기증자 관련 서비스
       </h2>
       <QuickLinkCard
+        icon={donate}
         title="기증희망등록 신청"
         description="기증희망등록은 누군가에게 새로운 시작이 될 수 있는 소중한 약속입니다."
         path="/"
         isNewTap={false}
       />
       <QuickLinkCard
+        icon={mailBox}
         title="생명 나눔 희망 우체통 서비스"
         description="기증자 유과족과 이식수혜자가 서로의 안부를 물으며 싹트는 생명 나눔의 기적 그 기행의 시작을 생명 나눔 작성 우체통이 함께합니다."
         path="https://koda1458.kr/post/mem/memLogin.c"
--- src/features/home/component/donation-service/QuickLinkCard.tsx
+++ src/features/home/component/donation-service/QuickLinkCard.tsx
@@ -2,23 +2,33 @@ import BtnShortcut from "@/assets/icon/btn-shortcut.svg";
 import { Link } from "react-router-dom";
 
 interface QuickLinkCardProps {
+  icon: string;
   title: string;
   description: string;
   path: string;
   isNewTap: boolean;
 }
 
-export default function QuickLinkCard({ title, description, path, isNewTap }: QuickLinkCardProps) {
+export default function QuickLinkCard({
+  icon,
+  title,
+  description,
+  path,
+  isNewTap,
+}: QuickLinkCardProps) {
   return (
     <article
-      className="flex flex-col items-center justify-between gap-g4 rounded-r6 bg-secondary-5 p-p8"
+      className="flex flex-col items-center justify-between gap-g4 rounded-r6 bg-primary-5 p-p8"
       aria-labelledby={`${title}-heading`}
     >
       <div className="flex w-full flex-col gap-g3">
-        <h2 id={`${title}-heading`} className="text-h-sm font-bold mobile:text-h-xs">
-          {title}
-        </h2>
-        <p className="text-b-sm text-gray-70">{description}</p>
+        <div className="flex items-center gap-g3">
+          <img src={icon} alt="" className="h-[48px] w-auto object-fill" />
+          <h2 id={`${title}-heading`} className="text-h-sm font-bold mobile:text-h-xs">
+            {title}
+          </h2>
+        </div>
+        <p className="text-b-sm text-gray-70 mobile:text-b-xs">{description}</p>
       </div>
       <div className="flex w-full justify-end">
         {isNewTap ? (
@@ -27,7 +37,7 @@ export default function QuickLinkCard({ title, description, path, isNewTap }: Qu
             target="_blank"
             rel="noopener noreferrer"
             aria-label={`${title} 바로가기`}
-            className="flex items-center gap-g1 text-gray-50 mobile:text-b-sm"
+            className="flex items-center gap-g1 text-b-sm text-gray-40"
           >
             바로가기
             <img src={BtnShortcut} alt="화살표 아이콘" className="h-icon2 w-icon2" />
@@ -36,7 +46,7 @@ export default function QuickLinkCard({ title, description, path, isNewTap }: Qu
           <Link
             to={path}
             aria-label={`${title} 바로가기`}
-            className="flex items-center gap-g1 text-gray-50 mobile:text-b-sm"
+            className="flex items-center gap-g1 text-b-sm text-gray-40"
           >
             바로가기
             <img src={BtnShortcut} alt="화살표 아이콘" className="h-icon2 w-icon2" />
