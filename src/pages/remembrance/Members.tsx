bb4dda94d13bb0d20dfd7929905b9b5c20fddb1c
ijisun <ijisun9292@gmail.com>
2025-06-10 23:57:45 +0900

    feat: textinput 컴포넌트에 value, onChange props 반영

--- src/pages/remembrance/Members.tsx
+++ src/pages/remembrance/Members.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import { useIsMobile } from "@/shared/hooks/useIsMobile";
 
-import clsx from "clsx";
-import PlusIcon from "@/assets/icon/btn-more.svg?react";
-
-import { Tab } from "@/shared/components/Tab";
+import { Label } from "@/shared/components/Label";
 import DatePicker from "@/shared/components/calendar/DatePicker";
 import TextInput from "@/shared/components/TextInput";
-import TopVisual from "@/shared/components/TopVisual";
 import { Button } from "@/shared/components/Button";
-
 import DonorCard from "@/features/members/component/DonorCard";
 
+import PlusIcon from "@/assets/icon/btn-more.svg?react";
+
 export default function Members() {
+  const [inputValue, setInputValue] = useState("");
+
   const isMobile = useIsMobile(768);
   const [range, setRange] = useState<{ from: Date | null; to: Date | null }>({
     from: null,
@@ -22,37 +21,27 @@ export default function Members() {
   return (
     <>
       {/* 상단 배너 & 탭 메뉴 */}
-      <div
-        className={clsx(
-          "absolute left-1/2 top-[390px] z-10 w-[1062px] -translate-x-1/2",
-          "mobile:top-[330px] mobile:w-[328px]",
-        )}
-      >
-        <Tab
-          tabs={[
-            { label: "기증자 추모관", content: "" },
-            { label: "하늘나라 편지", content: "" },
-            { label: "수혜자 편지", content: "" },
-            { label: "기증자 스토리", content: "" },
-          ]}
-        />
-      </div>
-      <TopVisual type="memorial" />
 
       <div className="mx-auto mb-g12 mt-[102px] max-w-[1280px] px-p10 mobile:mb-[60px] mobile:mt-[56px] mobile:px-p6">
         {/* 기증일 & 기증자 검색 영역 */}
         <div className="flex gap-g7 text-gray-90 mobile:flex-col mobile:gap-g5">
           <div className="flex items-center gap-g4 mobile:flex-col mobile:items-start mobile:gap-g3">
-            <span className="font-bold mobile:text-b-sm">기증일</span>
+            <Label size="m" weight="bold" children="기증일"></Label>
+            {/* <span className="font-bold mobile:text-b-sm">기증일</span> */}
             <DatePicker range={range} onRangeChange={setRange} />
           </div>
           <div className="flex items-center gap-g4 mobile:flex-col mobile:items-start mobile:gap-g3">
-            <span className="font-bold mobile:text-b-sm">기증자명</span>
-            <TextInput
-              id=""
-              placeholder="성함을 입력해주세요"
-              height={isMobile ? "small" : "medium"}
-            />
+            <Label size="m" weight="bold" children="기증자명"></Label>
+            {/* <span className="font-bold mobile:text-b-sm">기증자명</span> */}
+            <div className="w-[313px] mobile:w-full">
+              <TextInput
+                id=""
+                placeholder="성함을 입력해주세요"
+                height={isMobile ? "small" : "medium"}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+            </div>
           </div>
           <div className="mobile:flex mobile:justify-end">
             <Button
@@ -72,7 +61,7 @@ export default function Members() {
 
         {/* 추모 카드 영역 */}
         <div className="flex flex-col items-center gap-g8 mobile:gap-g5">
-          <ul className="flex flex-wrap gap-g6 mobile:gap-x-3 mobile:gap-y-g5">
+          <ul className="flex flex-wrap gap-g6 mobile:gap-x-g3 mobile:gap-y-g5">
             <DonorCard />
             <DonorCard />
             <DonorCard />
