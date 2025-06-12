52fca7213372ad29f970e198d0eea3402518963b
jiwonnayoung <lny1203@naver.com>
2025-06-12 14:14:15 +0900

    fix: 라우팅 경로 수정

--- src/pages/remembrance/RecipientLetterView.tsx
+++ src/pages/remembrance/RecipientLetterView.tsx
@@ -2,29 +2,33 @@ import { Description } from "@/shared/components/Description";
 import LetterContent from "@/features/remembrance/letter-view/components/LetterContent";
 import CommentArea from "@/shared/components/comment/CommentArea";
 import { recipientLetter } from "@/features/remembrance/letter-view/components/mockLetter";
+import { TopArea } from "@/shared/components/TopArea";
 
 function RecipientLetterView() {
   return (
-    <div className="mx-auto max-w-[1200px] mobile:px-p6">
-      <Description
-        startBefore="수혜자 편지는 이식으로 새생명을 살고 있는 분들의 이야기입니다. 언제 어디서나 시간과 장소에 구애받지 않고, 익명으로 작성이 가능합니다."
-        checkItems={[
-          "생명을 나눠준 기증자에 대한 예의를 지켜주시고 존중해주시기 바랍니다.",
-          "비방이나 욕설 등 분위기를 해치는 내용은 작성자에 의해 임의 삭제될 수 있습니다.",
-          "개인정보 노출을 자제해주세요.",
-          "이 게시판에 올린 글은 한국장기조직기증원 뉴스레터에 원문의 의미를 훼손하지 않는 범위내에서 교정을 거쳐 임의 수록할 수 있음을 양지하시기 바랍니다.",
-        ]}
-      />
-      <LetterContent
-        title={recipientLetter.title}
-        content={recipientLetter.content}
-        onGoList={() => console.log("목록 페이지로 이동")}
-        infoItems={recipientLetter.infoItems}
-        onEdit={() => console.log("편지 수정")}
-        onDelete={() => console.log("편지 삭제")}
-        mobileWidth="10rem"
-      />
-      <CommentArea />
+    <div className="mx-auto w-full">
+      <TopArea />
+      <div className="mx-auto mt-[76px] max-w-[1200px] mobile:px-p6">
+        <Description
+          startBefore="수혜자 편지는 이식으로 새생명을 살고 있는 분들의 이야기입니다. 언제 어디서나 시간과 장소에 구애받지 않고, 익명으로 작성이 가능합니다."
+          checkItems={[
+            "생명을 나눠준 기증자에 대한 예의를 지켜주시고 존중해주시기 바랍니다.",
+            "비방이나 욕설 등 분위기를 해치는 내용은 작성자에 의해 임의 삭제될 수 있습니다.",
+            "개인정보 노출을 자제해주세요.",
+            "이 게시판에 올린 글은 한국장기조직기증원 뉴스레터에 원문의 의미를 훼손하지 않는 범위내에서 교정을 거쳐 임의 수록할 수 있음을 양지하시기 바랍니다.",
+          ]}
+        />
+        <LetterContent
+          title={recipientLetter.title}
+          content={recipientLetter.content}
+          onGoList={() => console.log("목록 페이지로 이동")}
+          infoItems={recipientLetter.infoItems}
+          onEdit={() => console.log("편지 수정")}
+          onDelete={() => console.log("편지 삭제")}
+          mobileWidth="10rem"
+        />
+        <CommentArea />
+      </div>
     </div>
   );
 }
--- src/pages/remembrance/StoryView.tsx
+++ src/pages/remembrance/StoryView.tsx
@@ -2,27 +2,31 @@ import { Description } from "@/shared/components/Description";
 import LetterContent from "@/features/remembrance/letter-view/components/LetterContent";
 import CommentArea from "@/shared/components/comment/CommentArea";
 import { StoryLetter } from "@/features/remembrance/letter-view/components/mockLetter";
+import { TopArea } from "@/shared/components/TopArea";
 
 function StoryView() {
   return (
-    <div className="mx-auto max-w-[1200px] mobile:px-p6">
-      <Description
-        startBefore="한국장기조직기증원 장기구득 코디네이터가 함께 한 영원히 기억되는 기증자의 숭고한 나눔의 순간과 아름다운 이야기를 적는 공간입니다."
-        checkItems={[
-          "KODA 장기구득 코디네이터들의 공간입니다.",
-          "개인정보 노출의 우려가 있으니 게시글 작성 시 개인정보 등록은 자제하 여 주시기 바랍니다.",
-        ]}
-      />
-      <LetterContent
-        title={StoryLetter.title}
-        content={StoryLetter.content}
-        onGoList={() => console.log("목록 페이지로 이동")}
-        infoItems={StoryLetter.infoItems}
-        onEdit={() => console.log("편지 수정")}
-        onDelete={() => console.log("편지 삭제")}
-        mobileWidth="7rem"
-      />
-      <CommentArea />
+    <div className="mx-auto w-full">
+      <TopArea />
+      <div className="mx-auto mt-[76px] max-w-[1200px] mobile:px-p6">
+        <Description
+          startBefore="한국장기조직기증원 장기구득 코디네이터가 함께 한 영원히 기억되는 기증자의 숭고한 나눔의 순간과 아름다운 이야기를 적는 공간입니다."
+          checkItems={[
+            "KODA 장기구득 코디네이터들의 공간입니다.",
+            "개인정보 노출의 우려가 있으니 게시글 작성 시 개인정보 등록은 자제하 여 주시기 바랍니다.",
+          ]}
+        />
+        <LetterContent
+          title={StoryLetter.title}
+          content={StoryLetter.content}
+          onGoList={() => console.log("목록 페이지로 이동")}
+          infoItems={StoryLetter.infoItems}
+          onEdit={() => console.log("편지 수정")}
+          onDelete={() => console.log("편지 삭제")}
+          mobileWidth="7rem"
+        />
+        <CommentArea />
+      </div>
     </div>
   );
 }
