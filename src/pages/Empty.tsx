c0f53d6a476eb95a7fe613a09def761d8d20145e
ijisun <118454010+ijisun@users.noreply.github.com>
2025-06-12 19:59:16 +0900

    Feat: 기타 페이지 퍼블리싱 (#72)
    * feat: empty 페이지 퍼블리싱
    
    * feat: 이전/홈 이동 버튼 라우팅 적용
    
    * feat: error 페이지 퍼블리싱
    
    * refactor: 피드백 반영하여 수정
    
    * refactor: img 태그에 alt 속성 추가

--- src/pages/Empty.tsx
+++ src/pages/Empty.tsx
@@ -1,3 +1,75 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import clsx from "clsx";
+import { Button } from "@/shared/components/Button";
+
+import construction from "@/assets/images/under-construction.png";
+import Notification from "@/assets/icon/exclamation.svg?react";
+import { useIsMobile } from "@/shared/hooks/useIsMobile";
+
 export default function Empty() {
-  return <>Empty</>;
+  const isMobile = useIsMobile(768);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // 이전 페이지로 이동
+  const handleGoBack = () => {
+    // history stack이 비어있지 않으면 뒤로가기, 아니면 홈으로
+    if (location.key !== "default") {
+      navigate(-1);
+    } else {
+      navigate("/home");
+    }
+  };
+
+  // 홈으로 이동
+  const handleGoHome = () => {
+    navigate("/home");
+  };
+
+  return (
+    <section
+      className={clsx(
+        "flex w-full flex-col items-center pb-[500px] pt-[175px] text-gray-90",
+        "mobile:pb-[158px] mobile:pt-[68px]",
+      )}
+    >
+      <img
+        src={construction}
+        alt="준비중 안내 이미지"
+        className={clsx("h-[277px] w-[277px]", "mobile:h-[108px] mobile:w-[108px]")}
+      />
+      <h2
+        className={clsx(
+          "mt-[45px] flex items-center gap-g4 text-h-lg font-bold text-gray-80",
+          "mobile:mt-[42px] mobile:gap-g3 mobile:text-h-sm",
+        )}
+      >
+        <Notification
+          className={clsx("h-icon5 w-icon5 text-gray-80", "w-icon4 mobile:h-icon4")}
+          aria-hidden="true"
+        />
+        페이지 <span className="text-secondary-80">준비중</span> 입니다.
+      </h2>
+      <p
+        className={clsx(
+          "mb-[58px] mt-[12px] text-center",
+          "mobile:mb-[22px] mobile:mt-0 mobile:text-b-sm",
+        )}
+      >
+        보다 나은 서비스를 위해 작업중입니다.
+        <br />
+        곧 완성된 페이지를 만나실 수 있습니다.
+        <br />
+        감사합니다.
+      </p>
+      <div className={clsx("flex gap-g6", "mobile:gap-g5")}>
+        <Button size={isMobile ? "small" : "large"} variant="tertiary" onClick={handleGoBack}>
+          이전 페이지로
+        </Button>
+        <Button size={isMobile ? "small" : "large"} variant="primary" onClick={handleGoHome}>
+          홈으로 가기
+        </Button>
+      </div>
+    </section>
+  );
 }
---
+++ src/pages/Error.tsx
@@ -0,0 +1,51 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import clsx from "clsx";
+import { useIsMobile } from "@/shared/hooks/useIsMobile";
+import { Button } from "@/shared/components/Button";
+import Notification from "@/assets/icon/exclamation.svg?react";
+
+export default function Error() {
+  const isMobile = useIsMobile(768);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // 이전 페이지로 이동
+  const handleGoBack = () => {
+    // history stack이 비어있지 않으면 뒤로가기, 아니면 홈으로
+    if (location.key !== "default") {
+      navigate(-1);
+    } else {
+      navigate("/home");
+    }
+  };
+
+  // 홈으로 이동
+  const handleGoHome = () => {
+    navigate("/home");
+  };
+
+  return (
+    <section
+      className={clsx(
+        "flex w-full flex-col items-center gap-[56px] py-[100px] text-gray-90",
+        "mobile:gap-[36px]",
+      )}
+    >
+      <div className="flex flex-col items-center gap-g5">
+        <Notification className="h-icon6 w-icon6" aria-hidden="true" />
+        <div className="text-center">
+          <h2 className="text-h-lg font-bold text-gray-70">에러 코드</h2>
+          <p className="mobile:mt-pag-g3 text-b-md">에러 메시지</p>
+        </div>
+      </div>
+      <div className={clsx("flex gap-g6", "mobile:gap-g4")}>
+        <Button size={isMobile ? "medium" : "large"} variant="tertiary" onClick={handleGoBack}>
+          이전 페이지로
+        </Button>
+        <Button size={isMobile ? "medium" : "large"} variant="primary" onClick={handleGoHome}>
+          홈으로 가기
+        </Button>
+      </div>
+    </section>
+  );
+}
