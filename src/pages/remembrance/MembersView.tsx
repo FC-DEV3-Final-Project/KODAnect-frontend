54c2916526764076001fcb063f8e1177635d1fd7
jiwonnayoung <lny1203@naver.com>
2025-06-16 16:52:04 +0900

    chore: test code 제거

--- src/pages/remembrance/MembersView.tsx
+++ src/pages/remembrance/MembersView.tsx
@@ -1,18 +1,3 @@
-import { useState, useRef } from "react";
-import DatePicker from "@/shared/components/calendar/DatePicker";
-import { Label } from "@/shared/components/Label";
-
 export default function MembersView() {
-  const [range, setRange] = useState<{ from: Date | null; to: Date | null }>({
-    from: null,
-    to: null,
-  });
-  const fromRef = useRef<HTMLButtonElement>(null);
-  return (
-    <>
-      기증자 추모관 상세 페이지
-      <Label onClick={() => fromRef.current?.focus()}>시작일</Label>
-      <DatePicker range={range} onRangeChange={setRange} fromRef={fromRef} />
-    </>
-  );
+  return <>기증자 추모관 상세 페이지</>;
 }
