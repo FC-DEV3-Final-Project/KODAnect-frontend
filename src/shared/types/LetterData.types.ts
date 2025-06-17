e3c6825d0d93e42d0f684eec36d1d8a47b8ed07d
ijisun <ijisun9292@gmail.com>
2025-06-17 15:27:29 +0900

    feat: letterData 타입 정의

--- src/shared/types/LetterData.types.ts
+++ src/shared/types/LetterData.types.ts
@@ -1,16 +1,18 @@
-type LetterCardSize = "lg" | "sm";
-
-type InfoItem = {
-  label: string;
-  value: string;
-};
-
 export interface LetterData {
-  labelType?: "letter" | "story";
-  size?: LetterCardSize;
-  infoItems: InfoItem[];
   letterSeq: number;
+  labelType?: "letter" | "story";
+  size?: "lg" | "sm";
+  letterNumber: number;
+  infoItems: Array<{ label: string; value: string }>;
   title: string;
   date: string;
   views?: number;
+  toBase?: string;
+}
+
+export interface FetchDataResult {
+  mappedData: LetterData[];
+  hasNext: boolean;
+  nextCursor: number;
+  totalCount: number;
 }
