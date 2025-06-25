4aea0f571bc2a5ef694e43fac44ebd89c6827614
ijisun <118454010+ijisun@users.noreply.github.com>
2025-06-25 13:13:42 +0900

    fix: Navigation 수정

--- src/pages/remembrance/LettersForm.tsx
+++ src/pages/remembrance/LettersForm.tsx
@@ -170,7 +170,7 @@ export default function LettersForm() {
         },
       });
       console.log(`${isEdit ? "수정" : "등록"} 성공:`, response.data);
-      navigate(`${isEdit ? `/heavenLetters/letters-view/${letterSeq}` : "/heavenLetters/letters"}`);
+      navigate(`${isEdit ? `/remembrance/letters-view/${letterSeq}` : "/remembrance/letters"}`);
     } catch (error) {
       console.error(error);
     }
