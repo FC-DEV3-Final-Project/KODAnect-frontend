408fa8366b2a9297aaa24f63495bf1d231408dbb
ijisun <ijisun9292@gmail.com>
2025-07-01 11:59:56 +0900

    refactor: 프로덕션 환경에서 React Query Devtools 제외

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   </StrictMode>,
 );
