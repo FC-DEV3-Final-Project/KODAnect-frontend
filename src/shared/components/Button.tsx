20d5dc6710afbb380a93d59d6d40d78788bf6553
nani <pha3113@naver.com>
2025-07-01 13:35:43 +0900

    refactor: button style clsx 적용

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 type ButtonProps<E extends React.ElementType> = {
   type?: "button" | "submit" | "reset";
   variant?: "primary" | "secondary" | "tertiary";
@@ -56,13 +58,13 @@ export default function Button<E extends React.ElementType = "button">({
     },
   }[size];
 
-  const buttonStyles = `
-    ${baseStyles} 
-    ${disabled ? variantStyles.disabledStyle : variantStyles.style} 
-    ${sizeStyles.style} 
-    ${disabled ? "cursor-not-allowed" : ""}
-    ${className}
-  `;
+  const buttonStyles = clsx(
+    baseStyles,
+    disabled ? variantStyles.disabledStyle : variantStyles.style,
+    sizeStyles.style,
+    disabled && "cursor-not-allowed",
+    className,
+  );
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
     if (event.key === "Enter" || event.key === " ") {
