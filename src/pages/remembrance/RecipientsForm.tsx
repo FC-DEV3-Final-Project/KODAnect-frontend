d54ecc81cdc1791af67955a055ebeb05697bea7c
nani <pha3113@naver.com>
2025-07-01 17:45:17 +0900

    refactor: option 선언 위치 변경

--- src/pages/remembrance/RecipientsForm.tsx
+++ src/pages/remembrance/RecipientsForm.tsx
@@ -17,6 +17,25 @@ import Captcha from "@/shared/components/Captcha";
 import Button from "@/shared/components/Button";
 import CustomUploadAdapterPlugin from "@/shared/utils/ckeditor/CustomUploadAdapterPlugin";
 
+// 수혜장기
+const organOptions = [
+  { label: "신장", value: "ORGAN001" },
+  { label: "간장", value: "ORGAN002" },
+  { label: "췌장", value: "ORGAN003" },
+  { label: "심장", value: "ORGAN004" },
+  { label: "폐", value: "ORGAN005" },
+  { label: "췌도", value: "ORGAN006" },
+  { label: "소장", value: "ORGAN007" },
+  { label: "대장", value: "ORGAN008" },
+  { label: "위장", value: "ORGAN009" },
+  { label: "십이지장", value: "ORGAN010" },
+  { label: "비장", value: "ORGAN011" },
+  { label: "손,팔", value: "ORGAN012" },
+  { label: "안구", value: "ORGAN013" },
+  { label: "인체조직", value: "ORGAN014" },
+  { label: "직접입력", value: "ORGAN000" },
+];
+
 export default function RecipientsForm() {
   const [anonymityFlag, setAnonymityFlag] = useState(false);
   const [organCode, setOrganCode] = useState("");
@@ -37,25 +56,6 @@ export default function RecipientsForm() {
   const handleCheckboxChange = () => {
     setAnonymityFlag((prev) => !prev);
   };
-
-  // 수혜장기
-  const organOptions = [
-    { label: "신장", value: "ORGAN001" },
-    { label: "간장", value: "ORGAN002" },
-    { label: "췌장", value: "ORGAN003" },
-    { label: "심장", value: "ORGAN004" },
-    { label: "폐", value: "ORGAN005" },
-    { label: "췌도", value: "ORGAN006" },
-    { label: "소장", value: "ORGAN007" },
-    { label: "대장", value: "ORGAN008" },
-    { label: "위장", value: "ORGAN009" },
-    { label: "십이지장", value: "ORGAN010" },
-    { label: "비장", value: "ORGAN011" },
-    { label: "손,팔", value: "ORGAN012" },
-    { label: "안구", value: "ORGAN013" },
-    { label: "인체조직", value: "ORGAN014" },
-    { label: "직접입력", value: "ORGAN000" },
-  ];
 
   useEffect(() => {
     if (organCode !== "ORGAN000") {
